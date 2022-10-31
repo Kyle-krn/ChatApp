957408de10ea4e34693d902862a838812e414b39
Egor <egorjkee96@gmail.com>
2022-10-31 21:51:30 +0300

    connect postgres

--- IlStudioChat/settings.py
+++ IlStudioChat/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'IlStudioChat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chatdb',
+        'USER': 'chatuser',
+        'PASSWORD': 'pass123',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
