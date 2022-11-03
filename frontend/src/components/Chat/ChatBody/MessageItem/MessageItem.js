cd71d34694cee60a0b72f8c1bc1c45258257254a
Egor <egorjkee96@gmail.com>
2022-11-03 18:33:46 +0300

    fix times

--- frontend/src/components/Chat/ChatBody/MessageItem/MessageItem.js
+++ frontend/src/components/Chat/ChatBody/MessageItem/MessageItem.js
@@ -3,19 +3,18 @@ import styles from './MessageItem.module.css';
 import checkSVG from './../../../../../static/img/messageCheck.svg'
 import myBubbleSVG from './../../../../../static/img/Mybubbletip.png'
 import otherBubblSVG from './../../../../../static/img/otherBubbletip.png'
-import Moment from 'react-moment';
 
 export const MessageItem = React.memo(({myMess,id, username, message, created_at, isMyMessageUp, type}) => {
     let userTime = new Date(created_at.replace(' ', 'T')).valueOf() - ((new Date).getTimezoneOffset() * 60000)
     userTime = new Date(userTime)
+    let minutes = userTime.getMinutes().toString().length === 1? "0" + userTime.getMinutes(): userTime.getMinutes()
     return (
         <div message_id={id} className={`${styles.messageItem} ${myMess? styles.myMessage: styles.otherMessage}`}>
             <span className={styles.username}>{username}</span>
             <span className={styles.messageText}>{message}</span>
             <div className={styles.messageStatus}>
                 <span className={styles.messageTime}>
-                    {/* <Moment date={userTime} format="h:mm"/> */}
-                    {`${userTime.getHours()}:${userTime.getMinutes()}`}
+                    {`${userTime.getHours()}:${minutes}`}
                 </span>
                 <span className={styles.messageCheck}> 
                     <img src={checkSVG} loading="lazy"></img> 
